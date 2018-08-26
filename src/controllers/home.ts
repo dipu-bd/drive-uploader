6c9e382ea18a93480f06c64d1c91fdaa8ea51075
dipu-bd <dipu.sudipta@gmail.com>
2018-08-27 02:18:54 +0600

    Add new downloads

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -2,22 +2,22 @@ import { Request, Response } from 'express'
 import { GoogleDrive } from '../services/google-drive'
 import { IndexData } from '../models/index'
 import { ErrorData } from '../models/error'
+import { Downloader, DownloadItem } from '../services/downloader'
 
 export class HomeController {
   static async get(req: Request, res: Response) {
-    // make sure they are logged in
-    const token = req.cookies['_drive.token']
-    if (!token || !token.length) {
-      const auth = GoogleDrive.getAccessTokenUrl()
-      res.redirect(auth)
-      return
-    }
-    // generate page data
     try {
-      const data = {} as IndexData
-      data.token = token
+      const token = JSON.parse(req.cookies['_drive.token'])
       const drive = GoogleDrive.getInstance(token)
-      data.files = await drive.listFiles()
+      const downloader = Downloader.getInstance(drive)
+
+      if (req.query.url) {
+        const item = DownloadItem.createInstance(req.query.url)
+        downloader.addToQueue(item)
+      }
+
+      const data = {} as IndexData
+      data.items = downloader.list
       res.render('index', data)
     } catch (err) {
       res.render('error', { error: err, message: err.message } as ErrorData)
